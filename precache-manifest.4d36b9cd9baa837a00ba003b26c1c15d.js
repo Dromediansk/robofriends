e58bf37f9177f4104669a9310b25d8dbabacf20e
Dromediansk <pillar.mr@gmail.com>
2018-12-14 13:24:07 +0100

    Updates

---
+++ precache-manifest.4d36b9cd9baa837a00ba003b26c1c15d.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "12a3db8ba7f5423327bb12aed3c41977",
+    "url": "/robofriends/static/media/SEGA.12a3db8b.woff"
+  },
+  {
+    "revision": "1446d6ec194445c3572d",
+    "url": "/robofriends/static/js/runtime~main.1446d6ec.js"
+  },
+  {
+    "revision": "5de4db8c7f6452588ca9",
+    "url": "/robofriends/static/js/main.5de4db8c.chunk.js"
+  },
+  {
+    "revision": "f33326637eb20775571b",
+    "url": "/robofriends/static/js/1.f3332663.chunk.js"
+  },
+  {
+    "revision": "5de4db8c7f6452588ca9",
+    "url": "/robofriends/static/css/main.790c6206.chunk.css"
+  },
+  {
+    "revision": "f33326637eb20775571b",
+    "url": "/robofriends/static/css/1.66995f3f.chunk.css"
+  },
+  {
+    "revision": "af0233a6b49058cf8e2b02852bab9746",
+    "url": "/robofriends/index.html"
+  }
+];
